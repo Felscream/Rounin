211b2b2e9bc70b6ce25480254ed18bb1a5b3d7a2
Jérémy RASENDRASON <xblackfieldx@gmail.com>
2019-01-02 01:47:02 +0100

    More animation fixes

---
+++ Assets/Scripts/CharacterController/StateActions/IsGroundedNonPlacing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName = "Actions/State Actions/IsGroundedNonPlacing")]
+    public class IsGroundedNonPlacing : StateActions
+    {
+
+        public float GroundedDistance = .8f;
+        public float InAirDistance = .85f;
+        public override void Execute(StateManager states)
+        {
+            Vector3 origin = states.mTransform.position;
+            origin.y += .7f;
+            float d = states.IsGrounded ? GroundedDistance : InAirDistance;
+
+            RaycastHit hit;
+            
+            if (Physics.SphereCast(origin, 0.25f, -Vector3.up, out hit, d, Layers.IgnoreLayersIsGrounded))
+            {
+                states.IsGrounded = true;
+            }
+            else
+            {
+                states.IsGrounded = false;
+            }
+        }
+    }
+}
+
+
